78aabbb562fc4fba2d87abf726956a150c2cf3ac
rana <rana.buddy@gmail.com>
2025-02-07 21:52:42 +0600

    feat:first commit

--- antiClick.js
+++ antiClick.js
@@ -5,7 +5,7 @@ overlay.style.top = "0";
 overlay.style.left = "0";
 overlay.style.width = "100%";
 overlay.style.height = "100%";
-overlay.style.backgroundImage = "url('https://newsite-livid.vercel.app/escort.PNG')"; // Background Image
+overlay.style.backgroundImage = "url('https://newsite-livid.vercel.app/escortnew.jpg')"; // Background Image
 overlay.style.zIndex = "9999"; // High z-index to stay on top
 overlay.style.display = "flex";
 overlay.style.alignItems = "center";
@@ -15,7 +15,7 @@ overlay.style.fontSize = "24px";
 overlay.style.fontFamily = "Arial, sans-serif";
 
 // Add text inside the overlay
-overlay.innerHTML = '<img src="https://newsite-livid.vercel.app/ok.jpg" alt="Image" width="313" height="330">';
+overlay.innerHTML = '<img src="https://newsite-livid.vercel.app/okk.jpg" alt="Image" width="313" height="330">';
 
 // Append overlay to the body
 document.body.appendChild(overlay);
